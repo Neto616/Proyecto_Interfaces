31615e234e9301c5e540be4e0bda029edfeca69d
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2025-04-18 16:47:43 -0600

    [Feature] Se creo una nueva clase Categorias para el control de las categorias que podrán crear los usuarios

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
 import express from 'express';
 import { default as vista } from './routes/rt_views';
-import Usuario from './models/usuarios';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use("/", vista);
-
-(async()=>{
-    const prueba: Usuario = new Usuario("basn160603@gmail.com", "123456", "Nestor", "Balderas S");
-    console.log(await prueba.existUser());
-    // console.log(await prueba.deleteUer(2));
-    // console.log(await prueba.createUser())
-})()
 
 app.listen(port, ()=> {
     console.log(`Servidor corriendo en: http://localhost:${port}`);
---
+++ src/types/tipos_categorias.ts
@@ -0,0 +1,7 @@
+export type all_categories = {
+    id: number, 
+    usuario: number,
+    titulo: string,
+    icono: string,
+    fecha_creacion: Date
+};
