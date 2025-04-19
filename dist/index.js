ea26794b28b3081078960859dc2e2620674782fe
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2025-04-18 18:35:49 -0600

    Index

--- dist/index.js
+++ dist/index.js
@@ -1,28 +1,13 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const rt_views_1 = __importDefault(require("./routes/rt_views"));
-const categorias_1 = require("./models/categorias");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use("/", rt_views_1.default);
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    const categoria = new categorias_1.CategoriaRepository();
-    // const result = await categoria.getAll(4)
-    // console.log(result.info["data"])
-}))();
 app.listen(port, () => {
     console.log(`Servidor corriendo en: http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,6 @@ const port = process.env.PORT || 3000;
 
 app.use("/", vista);
 
-(async()=> {
-    const categoria: CategoriaRepository = new CategoriaRepository();
-    // const result = await categoria.getAll(4)
-    // console.log(result.info["data"])
-})()
-
 app.listen(port, ()=> {
     console.log(`Servidor corriendo en: http://localhost:${port}`);
 });
