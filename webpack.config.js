4e62bae45f3194a98fc2bf35595bd0bffe979e4e
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2025-05-13 09:21:18 -0600

    hola

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const pruebaLocal = true
+const pruebaLocal = 0
 module.exports = {
     entry: './app/src/index.jsx',
     output: {
